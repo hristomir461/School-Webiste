ebe86752f67f13f0ed22617564a8c19fabb42226
hristomir461 <hristomir.hari@gmail.com>
2024-04-25 05:07:05 +0300

    add htmls

---
+++ htmls/js/script.js
@@ -0,0 +1,36 @@
+let menu_icons = document.querySelector('.menu-icons'),
+nav = document.querySelector('nav');
+
+menu_icons.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
+
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+
+})(jQuery);
