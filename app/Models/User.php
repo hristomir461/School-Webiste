ff409a64e8b192d9dee36c6c2fdb026420133b1f
Hristomir <hristomir.hari@gmail.com>
2024-10-09 14:00:24 +0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,6 +46,6 @@ class User extends Authenticatable implements FilamentUser
 
       public function canAccessFilament(): bool
     {
-        return $this->email == 'risebellltd@gmail.com';
+        return $this->email == 'test@example.com';
     }
 }
